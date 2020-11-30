e9e272ad9da63096f98e41164d805297d540b3cb
Ferdinand Wijaya Susilo <43377805+Ferdi1314@users.noreply.github.com>
2020-11-30 23:56:47 +0700

    Add files via upload

---
+++ ApiPinjamRuang/ApiPinjamRuang/Controllers/HistoryController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiPinjamRuang.Models;
+using ApiPinjamRuang.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPinjamRuang.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HistoryController : ControllerBase
+    {
+        private readonly HistoryService _historyService;
+        public HistoryController(HistoryService historyService)
+        {
+            _historyService = historyService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<History>> Get()
+        {
+            return _historyService.Get();
+        }
+
+        [HttpGet("{id:length(24)}", Name ="GetHistory")]
+        public ActionResult<History> Get(string id)
+        {
+            var history = _historyService.Get(id);
+            if (history == null)
+            {
+                return NotFound();
+            }
+            return history;
+        }
+
+        [HttpPost]
+        public ActionResult<History> Create(History history)
+        {
+            _historyService.Create(history);
+            return CreatedAtRoute("GetBook", new { id = history.id.ToString() }, history);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, History newHistory)
+        {
+            var history = _historyService.Get(id);
+            if (history == null)
+            {
+                return NotFound();
+            }
+
+            _historyService.Update(id, newHistory);
+            return NoContent();
+        }
+    }
+}
---
+++ ApiPinjamRuang/ApiPinjamRuang/Models/History.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPinjamRuang.Models
+{
+    public class History
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        [BsonElement]
+        public string date { get; set; }
+        [BsonElement]
+        public string name { get; set; }
+        [BsonElement]
+        public string facility { get; set; }
+        [BsonElement]
+        public string eventName { get; set; }
+        [BsonElement]
+        public string pic { get; set; }
+        [BsonElement]
+        public string contact { get; set; }
+        [BsonElement]
+        public string nim { get; set; }
+        [BsonElement]
+        public int capacity { get; set; }
+        [BsonElement]
+        public int time { get; set; }
+        [BsonElement]
+        public int session { get; set; }
+    }
+}
